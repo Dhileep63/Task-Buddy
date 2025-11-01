507f42d9438025f26db918c2aecc7133fe3db939
Dhileep Nagamalli <144030639+Dhileep63@users.noreply.github.com>
2025-11-01 13:48:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from './pages/Home';
+import './style.css'
+function App() {
+  return (
+    <div className="app">
+      <h1>TASK BUDDY</h1>
+      
+      <Home />
+    </div>
+  );
+}
+
+export default App;
